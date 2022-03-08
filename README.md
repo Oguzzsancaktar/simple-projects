3be87fb47d5efc6f44df966673ab2bdd8d1e7a51
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-03-08 19:24:18 +0300

    47

--- README.md
+++ README.md
@@ -9,3 +9,19 @@
 <h5>In typescript we should install @types for installed lifraries</h5>
 
 - styled-component (@types/styled-components)
+
+### Implementation 
+- Add google fonts in public html.
+- We will use triviaAPI for questions.
+- Create components folder 
+- Create API.ts (Loading and fetching data for API)
+- Create utils.ts (Small functions)
+
+#### TypeScript 
+- We need to define types for events for example if we are taking event parameter on function we need to say event type and generic 
+- For taking props to component we need to define type Props and this is object which is store prop element types
+- We need to use type for function component (FC) and give here type Props
+- We can define enums in typescript 
+- Define like this => Export enum EnumName 
+- And use it like type : EnumName
+<h6>DangerouslySetInnerHTML</h6>
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+// Components
+import QuestionCard from "./components/QuestionCard";
 
-function App() {
-  return <div className="App">Quiz</div>;
-}
+// API
+import { Difficulty, fetchQuizQuestions, Question, QuestionState } from "./API";
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>React Quiz</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className="score">Score: </p> : null}
+      {loading && <p>Loading Questions...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          question={questions[number].question}
+          answers={questions[number].answers}
+          callback={checkAnswer}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+        />
+      )}
+
+      {!gameOver && !loading && userAnswers.length === number + 1 && (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default App;
